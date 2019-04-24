3862ff26e8922aa56e84d97ecafd70af391d335e
wathiwut193 <wathiwut193@gmail.com>
2019-04-24 15:13:33 +0700

    spell function not complete and change structure program

---
+++ word_tokenize.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import deepcut
+
+
+def run(text):
+    text = get_news(text)
+    # text = word_segment(text)
+    return text
+
+
+def get_news(text):
+    web_token = requests.get(text)
+    html_parser = BeautifulSoup(web_token.text, 'html.parser')
+
+    paragraphs = html_parser.find_all("p")
+    content = []
+    for p in paragraphs:
+        content.append(p.getText())
+        text = "\n".join(content)
+    return text
+
+
+def word_segment(text):
+    text = deepcut.tokenize(text, custom_dict="custom_dict/custom_dict.txt")
+    text = "|".join(text)
+    return text
