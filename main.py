588303da64ac46afa8e415a6ea52deb696d090fc
wathiwut193 <wathiwut193@gmail.com>
2019-04-24 13:36:30 +0700

    edit tag to <thai language> and and add spell checker function

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ Created on Mon Apr 22 14:15:00 2019
 
 import requests
 from bs4 import BeautifulSoup
-
+import deepcut
 import time
 from pymongo import MongoClient
 import os
 import glop
+import re
 import tag_object as tag
 
 
@@ -21,22 +22,82 @@ import tag_object as tag
 def get_news(URL):
     data = requests.get(URL)
     soup = BeautifulSoup(data.text, 'html.parser')
-    header = soup.find_all("h1")
-    date_news = soup.find_all("div", {"class": "css-1cxbv8p evs3ejl7"})
-    content = soup.find_all("p")
+    # header = soup.find_all("h1")
+    # date_news = soup.find_all("div", {"class": "css-1cxbv8p evs3ejl7"})
+    paragraphs = soup.find_all("p")
 
-    text = ''
-    # for i in header:
-    #   text += i.text
-    # for z in date_news:
-    #    text += z.text
-    for j in content:
-        text += j.text
+    # text = ''
+    # # for i in header:
+    # #   text += i.text
+    # # for z in date_news:
+    # #    text += z.text
+    # for j in paragraphs:
+    #     text += j.get_text()
 
-    return text
+    content = []
+    for p in paragraphs:
+        content.append(p.getText())
+        all_content = "\n".join(content)
+    return all_content
+
+
+def word_tokenize(text):
+    text = deepcut.tokenize(text, custom_dict="custom_dict/custom_dict.txt")
+    word_cut = "|".join(text)
+
+    regex = r"(<[^<]เวลา>[^<]*</[^<]เวลา>)"
+    read_str = ("คดีฆ่าพระ ยังอึมครึม วอนช่วยแก้ปัญหาดับไฟใต้!\n"
+                "โดย ไทยรัฐฉบับพิมพ์<date>24 ม.ค. 2562</date> <time>04:30 น.</time>\n"
+                "สมภารวัดดัง จ.นราธิวาส ระบุสถานการณ์ในพื้นที่ยังอึมครึม ออกจากวัดไปกิจนิมนต์แต่ละครั้งไม่รู้จะมีชีวิตกลับมาหรือไม่ ผวาหนักถึงขั้นลูกศิษย์ต้องพกปืนป้องกันตัว ค้านแนวคิด ผบ.ทบ.ที่จะให้ทหารบวชพระ ชี้หัวดื้อไม่ยอมปฏิบัติศาสนกิจ ขณะที่พระสงฆ์ใน อ.สุไหงปาดี ออกบิณฑบาตวันแรกหลังเกิดเหตุยิงถล่มวัดทำพระมรณภาพ 2 รูป ท่ามกลางการรักษาความปลอดภัยเข้มงวด ด้านชาวพุทธ 3 จังหวัดชายแดนใต้กว่า 600 คน ตั้งขบวนแห่เรียกร้องสันติ\n")
+    matches = re.finditer(regex, read_str, re.MULTILINE)
+    match_i = []
+    for matchNum, match in enumerate(matches, start=1):
+        match_i.append(match.start())  # เก็บตำแหน่ง tag ตัวแรกที่เจอ
+        match_i.append(match.end())  # เก็บตำแหน่ง tag ตัวสุดท้ายที่เจอ
+
+        str_s = ''  # สร้างตัวแปรมาเก็บ string ที่ไม่มี tag
+        index_match = 0  # สร้างตัวแปรเพื่อเช็ค index match_i
+        str_tag = ''  # สร้างตัวแปรมาเก็บ string ที่มี tag
+    for i in range(len(read_str)):
+        if index_match <= len(match_i):
+            if index_match % 2 == 0:  # if index_match % 2 เท่ากับ 0
+                if index_match < len(match_i):  # if index_match < length for match_i
+                    if match_i[index_match] == i:  # if match_i[index_match] = i
+                        index_match = index_match + 1  # ให้ทำการเพิ่มค่า index_match
+                        str_tag += read_str[i]  # ใส่ข้อมูลแรกของ tag ลงไปใน str_tag
+                        str_s += ' ='  # ใส่ช่องว่างให้ตัวสุดท้ายก่อนที่จะเจอ tag
+                    else:
+                        str_s += read_str[i]  # else ให้เอาค่า str ตำแหน่ง i ไปใส่ใน str_s
+                else:
+                    str_s += read_str[i]  # เก็บ str แถวสุดท้ายหลังจาก tag
+            elif index_match % 2 != 0:  # if index_match % 2 ไม่เท่ากับ 0
+                if match_i[index_match] == i:  # if match_i[index_match] = i
+                    index_match = index_match + 1  # ให้ทำการเพิ่มค่า index_match
+                    str_tag += '\n'  # ใส่ช่องว่างให้ตัวสุดท้ายให้หลัง tag
+                else:
+                    str_tag += read_str[i]  # else ให้เอาค่า str ตำแหน่ง i ไปใส่ใน str_tag
+
+    tag_split = (str_tag.split('\n'))
+    word_cut = deepcut.tokenize(str_s, custom_dict='corpus/compond_word.txt')
+    ind = 0
+    for i in range(len(word_cut)):
+        if word_cut[i] == '=':
+            word_cut[i] = tag_split[ind]
+            ind = ind + 1
+
+    word = ''
+    for i in range(len(word_cut)):
+        if word_cut[i] != '=':
+            word = ('|'.join(word_cut))
+
+    return word
 
 
 if __name__ == '__main__':
     URL = input("Enter URL:")
     news = get_news(URL)
+    news = word_tokenize(news)
+
+    news_none_cut = get_news(URL)
+    print(tag.tag_start(news_none_cut))
     print(tag.tag_start(news))
---
+++ spell_checking.py
@@ -0,0 +1,252 @@
+import regex
+import time
+from allplace import get_json_data
+
+
+def spell_checker(spell):
+    spell = province_fail(spell)
+    spell = province_amphoe_fail(spell)
+    spell = province_tambon_fail(spell)
+    spell = amphoe_tambon_fail(spell)
+    spell = province_amphoe_fail_tambon_fail(spell)
+    # read_text = tag_......
+    return spell
+
+
+def province_fail(pro_matches, Data):
+    province_set = set()
+    province_set.add('กรุงเทพฯ')
+    for i in pro_matches:
+        for k in Data:
+            for att, val in k.items():
+                if att == 'province':
+                    province_set.add(val)
+    province_list = list(province_set)
+    res = find_result(province_list, pro_matches)
+    return res
+
+
+def province_amphoe_fail(pro_amp_matches, Data):
+    amphoe_set = set()
+    for i in range(len(pro_amp_matches)):
+        for j in range(len(pro_amp_matches[i])):
+            if pro_amp_matches and j != len(pro_amp_matches[i]) - 1:
+                # print(pro_amp_matches[i][j])
+                for k in Data:
+                    for att, val in k.items():
+                        if att == 'province' and val == pro_amp_matches[i][j]:
+                            # print(val)
+                            for att, val in k.items():
+                                if att == 'amphoe':
+                                    amphoe_set.add(val)
+
+    # print(amphoe_set)
+    amphoe_list = list(amphoe_set)
+    res = find_result(amphoe_list, pro_amp_matches[0][1])
+    return res
+
+
+def province_tambon_fail(pro_tam_matches, Data):
+    tambon_set = set()
+
+    for i in range(len(pro_tam_matches)):
+        for j in range(len(pro_tam_matches[i])):
+            if pro_tam_matches and j != len(pro_tam_matches[i]) - 1:
+                for k in Data:
+                    for att, val in k.items():
+                        if att == 'province' and val == pro_tam_matches[i][j]:
+                            for att, val in k.items():
+                                if att == 'district':
+                                    tambon_set.add(val)
+
+    tambon_list = list(tambon_set)
+    res = find_result(tambon_list, pro_tam_matches[0][1])
+    return res
+
+
+def amphoe_tambon_fail(amp_tam_matches, Data):
+    tambon_set = set()
+
+    for i in range(len(amp_tam_matches)):
+        for j in range(len(amp_tam_matches[i])):
+            if amp_tam_matches and j != len(amp_tam_matches[i]) - 1:
+                for k in Data:
+                    for att, val in k.items():
+                        if att == 'amphoe' and val == amp_tam_matches[i][j]:
+                            for att, val in k.items():
+                                if att == 'district':
+                                    tambon_set.add(val)
+
+    tambon_list = list(tambon_set)
+    res = find_result(tambon_list, amp_tam_matches[0][1])
+    return res
+
+
+def province_amphoe_fail_tambon_fail(pro_amp_tam_matches, Data):
+    amphoe_set = set()
+    tambon_set = set()
+    for i in range(len(pro_amp_tam_matches)):
+        for j in range(len(pro_amp_tam_matches[i])):
+            if pro_amp_tam_matches and j != len(pro_amp_tam_matches[i]) - 1:
+                for k in Data:
+                    for att, val in k.items():
+                        if att == 'province' and val == pro_amp_tam_matches[i][0]:
+                            for att, val in k.items():
+                                if att == 'amphoe':
+                                    amphoe_set.add(val)
+
+                for k in Data:
+                    for att, val in k.items():
+                        if att == 'province' and val == pro_amp_tam_matches[i][0]:
+                            for att, val in k.items():
+                                if att == 'district':
+                                    tambon_set.add(val)
+
+    # print(amphoe_set)
+    amphoe_list = list(amphoe_set)
+    res1 = find_result(amphoe_list, pro_amp_tam_matches[i][1])
+    # print(district_set)
+    tambon_list = list(tambon_set)
+    res2 = find_result(tambon_list, pro_amp_tam_matches[i][2])
+    return res1, res2
+
+
+def dict_action():
+    with open('corpus/Action.txt', 'r', encoding='utf8') as read_file:
+        dict = read_file.read()
+        list_dict = dict.split('\n')
+        return list_dict
+
+
+def dict_hospital():
+    with open('corpus/hospital.txt', 'r', encoding='utf8') as read_file:
+        dict = read_file.read()
+        list_dict = dict.split('\n')
+        return list_dict
+
+
+def dict_country():
+    with open('corpus/country.txt', 'r', encoding='utf8') as read_file:
+        dict = read_file.read()
+        list_dict = dict.split('\n')
+        return list_dict
+
+
+def dict_store():
+    with open('corpus/store.txt', 'r', encoding='utf8') as read_file:
+        dict = read_file.read()
+        list_dict = dict.split('\n')
+        return list_dict
+
+
+def dict_university():
+    with open('corpus/university.txt', 'r', encoding='utf8') as read_file:
+        dict = read_file.read()
+        list_dict = dict.split('\n')
+        return list_dict
+
+
+def Autocorrection(x):
+    Data = get_json_data()
+
+    action_fail = r"<กระทำ_ผิด>([^<]*)</กระทำ_ผิด>"
+    hospital_fail = r"<โรงพยาบาล_ผิด>([^<]*)</โรงพยาบาล_ผิด>"
+    country_fail = r"<ประเทศ_ผิด>([^<]*)</ประเทศ_ผิด>"
+    store_fail = r"<ห้าง_ผิด>([^<]*)</ห้าง_ผิด>"
+    university_fail = r"<มหาวิทยาลัย_ผิด>([^<]*)</มหาวิทยาลัย_ผิด>"
+    pro = r"<จังหวัด_ผิด>([^<]*)</จังหวัด_ผิด>"
+    pro_amp = r"<จังหวัด>([^<]*)</จังหวัด>[^<]*<อำเภอ_ผิด>([^<]*)</อำเภอ_ผิด>(?!<ตำบล_ผิด>)"
+    pro_tam = r"<จังหวัด>([^<]*)</จังหวัด>[^<]*<ตำบล_ผิด>([^<]*)</ตำบล_ผิด>"
+    amp_tam = r"<อำเภอ>([^<]*)</อำเภอ>[^<]*<ตำบล_ผิด>([^<]*)</ตำบล_ผิด>"
+    pro_amp_tam = r"<จังหวัด>([^<]*)</จังหวัด>[^<]*<อำเภอ_ผิด>([^<]*)</อำเภอ_ผิด>[^<]*<ตำบล_ผิด>([^<]*)</ตำบล_ผิด>"
+
+    action_fail_matches = regex.findall(action_fail, x)
+    hospital_fail_matches = regex.findall(hospital_fail, x)
+    country_fail_matches = regex.findall(country_fail, x)
+    store_fail_matches = regex.findall(store_fail, x)
+    university_fail_matches = regex.findall(university_fail, x)
+    pro_matches = regex.findall(pro, x)
+    pro_amp_matches = regex.findall(pro_amp, x)
+    pro_tam_matches = regex.findall(pro_tam, x)
+    amp_tam_matches = regex.findall(amp_tam, x)
+    pro_amp_tam_matches = regex.findall(pro_amp_tam, x)
+
+    if pro_amp_tam_matches:
+        # province_amphoe_fail_tambon_fail(pro_amp_tam_matches,Data)
+        for i in pro_amp_tam_matches:
+            correct_x = regex.sub(r'<จังหวัด>' + i[0] + '</จังหวัด>', r'<จังหวัด>' + i[0] + '</จังหวัด>', x)
+            x = correct_x
+            correct_x = regex.sub(r'<อำเภอ_ผิด>' + i[1] + '</อำเภอ_ผิด>',
+                                  r'<อำเภอ>' + province_amphoe_fail_tambon_fail(pro_amp_tam_matches, Data)[
+                                      0] + '</อำเภอ>', x)
+            x = correct_x
+            correct_x = regex.sub(r'<ตำบล_ผิด>' + i[2] + '</ตำบล_ผิด>',
+                                  r'<ตำบล>' + province_amphoe_fail_tambon_fail(pro_amp_tam_matches, Data)[
+                                      1] + '</ตำบล>', x)
+            x = correct_x
+
+    if pro_amp_matches:
+        # province_amphoe_fail(pro_amp_matches,Data)
+        for i in pro_amp_matches:
+            correct_x = regex.sub(r'<จังหวัด>' + i[0] + '</จังหวัด>', r'<จังหวัด>' + i[0] + '</จังหวัด>', x)
+            x = correct_x
+            correct_x = regex.sub(r'<อำเภอ_ผิด>' + i[1] + '</อำเภอ_ผิด>',
+                                  r'<อำเภอ>' + province_amphoe_fail(pro_amp_matches, Data) + '</อำเภอ>', x)
+            x = correct_x
+
+    if pro_tam_matches:
+        # province_district_fail(pro_dis_matches,Data)
+        for i in pro_tam_matches:
+            correct_x = regex.sub(r'<จังหวัด>' + i[0] + '</จังหวัด>', r'<จังหวัด>' + i[0] + '</จังหวัด>', x)
+            x = correct_x
+            correct_x = regex.sub(r'<ตำบล_ผิด>' + i[1] + '</ตำบล_ผิด>',
+                                  r'<ตำบล>' + province_tambon_fail(pro_tam_matches, Data) + '</ตำบล>', x)
+            x = correct_x
+    if amp_tam_matches:
+        # amphoe_district_fail(amp_dis_matches,Data)
+        for i in amp_tam_matches:
+            correct_x = regex.sub(r'<อำเภอ>' + i[0] + '</อำเภอ>', r'<อำเภอ>' + i[0] + '</อำเภอ>', x)
+            x = correct_x
+            correct_x = regex.sub(r'<ตำบล_ผิด>' + i[1] + '</ตำบล_ผิด>',
+                                  r'<ตำบล>' + amphoe_tambon_fail(amp_tam_matches, Data) + '</ตำบล>', x)
+            x = correct_x
+    if pro_matches:
+        # province_fail(pro_matches,Data)
+
+        for i in pro_matches:
+            correct_x = regex.sub(r'<จังหวัด_ผิด>' + i + '</จังหวัด_ผิด>',
+                                  r'<จังหวัด>' + province_fail(i, Data) + '</จังหวัด>', x)
+            x = correct_x
+
+    if action_fail_matches:
+        for i in action_fail_matches:
+            res = find_result(dict_action(), i)
+            correct_x = regex.sub(r'<กระทำ_ผิด>' + i + '</กระทำ_ผิด>', '<กระทำ>' + res + '</กระทำ>', x)
+            x = correct_x
+
+    if hospital_fail_matches:
+        for i in hospital_fail_matches:
+            res = find_result(dict_hospital(), i)
+            correct_x = regex.sub(r'<โรงพยาบาล_ผิด>' + i + '</โรงพยาบาล_ผิด>', '<โรงพยาบาล>' + res + '</โรงพยาบาล>', x)
+            x = correct_x
+
+    if country_fail_matches:
+        for i in country_fail_matches:
+            res = find_result(dict_country(), i)
+            correct_x = regex.sub(r'<ประเทศ_ผิด>' + i + '</ประเทศ_ผิด>', '<ประเทศ>' + res + '</ประเทศ>', x)
+            x = correct_x
+
+    if store_fail_matches:
+        for i in store_fail_matches:
+            res = find_result(dict_store(), i)
+            correct_x = regex.sub(r'<ห้าง_ผิด>' + i + '</ห้าง_ผิด>', '<ห้าง>' + res + '</ห้าง>', x)
+            x = correct_x
+
+    if university_fail_matches:
+        for i in university_fail_matches:
+            res = find_result(dict_university(), i)
+            correct_x = regex.sub(r'<มหาวิทยาลัย_ผิด>' + i + '</มหาวิทยาลัย_ผิด>',
+                                  '<มหาวิทยาลัย>' + res + '</มหาวิทยาลัย>', x)
+            x = correct_x
+
+    return x
